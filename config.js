d1d8f23763848839c491f5a4a4ae94707fa4f740
dat17122002 <160883404+dat17122002@users.noreply.github.com>
2024-02-22 22:23:23 +0700

    Add files via upload
    Create Casper

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/qiyanax17';
+const eventName = '<3';
+const titleCard = 'Gửi em';
+const contentCard = '"Em là ánh sáng, niềm vui và ý nghĩa của cuộc đời tôi. Yêu thương em mãi mãi."';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
